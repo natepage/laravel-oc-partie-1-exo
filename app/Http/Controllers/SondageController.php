a1067587d15554eda18e6c0ebd1800decc8e7d78
N@thix86 <nathan.page86@gmail.com>
2015-12-22 22:41:46 +0100

    Mise en conformité du code du controller avec les consignes de l'exercice.

--- app/Http/Controllers/SondageController.php
+++ app/Http/Controllers/SondageController.php
@@ -12,14 +12,14 @@ class SondageController extends Controller
 	/**
 	 * Instance de SondageGestion
 	 *
-	 * @var Lib\Gestion\SondageGestion
+	 * @var \App\Gestion\SondageGestion
 	 */
 	protected $sondageGestion;
 
 	/**
 	 * Crée une nouvelle instance de SondageController
 	 *
-	 * @param Lib\Validation\SondageGestion $sondageGestion
+	 * @param \App\Gestion\SondageGestion $sondageGestion
 	 * @return void
 	 */
 	public function __construct(SondageGestion $sondageGestion)
@@ -37,6 +37,8 @@ class SondageController extends Controller
 	{
 		// Ici on doit retourner la vue "index" en lui transmettant un paramètre "sondage" contenant les sondage
 		// C'est la méthode "getSondages" de la gestion qui est chargée de livrer les éléments de ces sondages
+
+		return view('index')->with('sondages', $this->sondageGestion->getSondages());
 	}
 
 	/**
@@ -50,13 +52,18 @@ class SondageController extends Controller
 		// Ici on doit envoyer la vue "sondage" qui contient le formulaire du sondage
 		// C'est la méthode "getSondage" de la gestion qui est chargée de livrer les informations du sondage
 		// On doit transmettre 2 paramètres à la vue : "sondage" pour les informations du sondage et "nom" pour le nom du sondage
+
+		return view('sondage', [
+			'sondage' => $this->sondageGestion->getSondage($nom),
+			'nom' => $nom
+		]);
 	}
 
 	/**
 	 * Traitement du formulaire de vote
 	 *
 	 * @param  string $nom
-	 * @param  App\Http\Requests\SondageRequest $request
+	 * @param  \App\Http\Requests\SondageRequest $request
 	 * @return Redirect
 	 */
 	public function postVote($nom, SondageRequest $request)
@@ -71,12 +78,22 @@ class SondageController extends Controller
 			// - "sondage" pour les informations du sondage 
 			// - "resultats" pour les résultats du sondage 
 			// - "nom" pour le nom du sondage
+
+			return view('resultats', [
+				'sondage' => $this->sondageGestion->getSondage($nom),
+				'resultats' => $this->sondageGestion->getResults($nom),
+				'nom' => $nom
+			]);
 		}
 
 		// Ici comme l'Email a déjà été utilisé on doit rediriger sur la même requête avec la méthode "back" de la classe Redirect
 		// On doit transmettre en session flash avec le nom "error" l'informations
 		// "Désolé mais cet Email a déjà été utilisé pour ce sondage !"
 		// On doit transmettre aussi les anciennes saisies
+
+		return redirect()->back()
+						 ->with('error', 'Désolé mais cet Email a déjà été utilisé pour ce sondage !')
+						 ->withInput();
 	}
 
 }
